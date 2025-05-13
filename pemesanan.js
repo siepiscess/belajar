367f37d198811896041c91b5d6fbba8a1cb0b852
siepiscess <rani.krismantari6@smk.belajar.id>
2025-05-13 23:18:59 +0700

    Delete pemesanan.js

--- pemesanan.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const pemesananForm = document.getElementById("pemesananForm");
-
-    pemesananForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Form Pemesanan berhasil dikirim!");
-        pemesananForm.reset();
-    });
-});
