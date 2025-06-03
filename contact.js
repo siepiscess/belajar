7ab9572a69abc9d0203534cb960d9c2a49ece05e
siepiscess <rani.krismantari6@smk.belajar.id>
2025-06-03 21:35:38 +0700

    Delete contact.js

--- contact.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const contactForm = document.getElementById("contactForm");
-
-    contactForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Pesan Anda telah dikirim!");
-        contactForm.reset();
-    });
-});
